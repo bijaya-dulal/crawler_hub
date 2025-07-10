b5a89dae5af02d11e2675c969a415246157d07d6
bijaya-dulal <dulalbijaya0@gmail.com>
2025-07-10 19:15:53 +0545

    jwt tokwn verification

--- app/auth.py
+++ app/auth.py
@@ -2,6 +2,7 @@ from passlib.context import CryptContext # type: ignore
 from datetime import datetime, timedelta
 from jose import JWTError, jwt # type: ignore
 from typing import Optional
+from crawler_hub.app.schemas import TokenData
 
 # Password hashing setup
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -9,9 +10,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # JWT settings
 SECRET_KEY = "9f8d4e2e1b6a4a1e5a8e6f7c4d3b2a1f9c8d7e6f5a4b3c2d1e0f9a8b7c6d5e4f" #generated randomly and added here
 ALGORITHM = "HS256"
-
 #expires after 30 minutes
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 # Hash a password
 def hash_password(password: str) -> str:
@@ -28,3 +29,14 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+# Decode and verify JWT token
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            return None
+        return TokenData(email=email)
+    except JWTError:
+        return None
