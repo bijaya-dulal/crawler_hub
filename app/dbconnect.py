e05ee7df47fc31ed23b56a09c6a0d66884907eea
bijaya-dulal <dulalbijaya0@gmail.com>
2025-07-10 16:14:10 +0545

    user table created

---
+++ app/dbconnect.py
@@ -0,0 +1,8 @@
+#this is used only once to create the table in db if 
+# if you run twice it wont create the duplicate just deal with new tables 
+
+from database import engine, Base
+from models import User # import all  models here 
+
+Base.metadata.create_all(bind=engine)
+print("Tables created successfully!")
