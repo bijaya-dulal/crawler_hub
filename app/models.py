60f0540dd148d1b9d6eb30f8ce36c74d07ca74a2
bijaya-dulal <dulalbijaya0@gmail.com>
2025-07-10 16:26:05 +0545

    todos model created

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, DateTime, func
 from database import Base
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = "users"
@@ -8,3 +9,20 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     full_name = Column(String, nullable=True)
+
+    # Relationship: One user → many todos
+    todos = relationship("Todo", back_populates="user", cascade="all, delete")
+
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    task = Column(String, nullable=False)
+    completed = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    # Relationship: Each todo belongs to one user
+    user = relationship("User", back_populates="todos")
